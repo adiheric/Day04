b379dba1a1dd3b6835752f1d1c1230a3bba0b92b
adiheric <hadiheric@gmail.com>
2021-03-12 01:46:53 +0100

    declare string

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+var myFirstName = "Adi";
+var myLastName = "Heric";
+var myHomeTown = "Konjic";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
